9dd2a65d4b8f73bd07829ef91901b59a3012cec0
Loïc Dumay <dumay.famille@gmail.com>
2023-12-27 00:34:34 +0100

    Refactoring

--- src/Controller/PdfGeneratorController.php
+++ src/Controller/PdfGeneratorController.php
@@ -18,8 +18,14 @@ use Drupal\Core\Controller\ControllerBase;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\pdf_generator_drupal_module\Utils\UtilsFolderAndFiles;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use ErrorException;
+
+// Ancien module de génération de PDF
+use TCPDF;
+// Futur remplacant de TCPDF
 use pdf_generator\PdfGenerator;
+
 
 
 /**
@@ -206,5 +212,55 @@ class PdfGeneratorController extends ControllerBase {
 
 		// Success message.
 		\Drupal::logger('PDF Generator')->notice('pdf created');
+	}
+
+	// = = = = [ By Chen ] = = = =
+	public function exportPdfByChen($node): Response {
+		// Vérifiez si le nœud existe et a le type de contenu requis.
+		$node = \Drupal::entityTypeManager()->getStorage('node')->load($node);
+		if (!$node || ($node->getType() !== 'article' && $node->getType() !== 'article_image')) {
+			throw new NotFoundHttpException();
+		}
+		// Générez le contenu PDF.
+		$article = $this->getArticleContentByChen($node);
+		$pdfContent = $this->generatePdfByChen($article);
+
+		// Créez une réponse avec le contenu PDF.
+		$response = new Response($pdfContent);
+
+		// Configurez les en-têtes appropriés pour le PDF.
+		$response->headers->set('Content-Type', 'application/pdf');
+		$response->headers->set('Content-Disposition', 'attachment; filename="article.pdf"');
+
+		// Retournez la réponse.
+		return $response;
+	}
+
+	public function getArticleContentByChen($entity): array {
+		// get content
+		$title = $entity->getTitle();
+		$content = $entity->get('body')->value;
+
+		// Replace &nbsp; entities with spaces.
+		$content = str_replace('&nbsp;', ' ', $content);
+		// Remove HTML tags from the content.
+		$content = strip_tags($content);
+		return [
+			'title'=>$title,
+			'content' => $content,
+		];
+	}
+
+	private function generatePdfByChen(array $article): string {
+		// Generate the PDF content using TCPDF.
+		$pdf = new TCPDF();
+		$pdf->AddPage();
+		$pdf->SetFont('helvetica', '', 12);
+		$pdf->Write(0, $article['title'], '', 0, 'L', true, 0, false, false, 0);
+		$pdf->Ln(10); // Add some space between title and content
+		$pdf->Write(0, $article['content'], '', 0, 'L', true, 0, false, false, 0);
+
+		// Return the PDF content as a string.
+		return $pdf->Output('article.pdf', 'S');
 	}
 }
